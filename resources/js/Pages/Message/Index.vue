1c504949c6af229c7621b8e89aeb60f7f27e1b2f
Ilya <illyaetoilysha@gmail.com>
2023-11-14 18:56:00 +0200

    Making page, controller and route for message page

---
+++ resources/js/Pages/Message/Index.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container mx-auto my-4 px-4">
+        <h1>Message</h1>
+        <div class="my-3">
+            <div>
+                <input class="rounded-2xl" type="text" placeholder="Message">
+            </div>
+            <div>
+                <button href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl my-3">
+                    Send
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
