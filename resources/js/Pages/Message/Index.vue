9b3a56f24f6175007b91680f643a33f9909ecdad
Ilya <illyaetoilysha@gmail.com>
2023-11-15 12:40:36 +0200

    Adding messages

--- resources/js/Pages/Message/Index.vue
+++ resources/js/Pages/Message/Index.vue
@@ -1,25 +1,41 @@
 <script setup>
-defineProps({
+import {ref} from "vue";
+
+const props = defineProps({
     'messages': {
         type: Array
     }
-})
+});
+
+const message = ref('');
+
+function send() {
+    axios.post(route('message.store'), {
+        message: message.value
+    }).then(response => {
+        props.messages.unshift(response.data);
+    }).catch(error => {
+        console.log(error);
+    });
+
+    message.value = '';
+}
 </script>
 
 <template>
     <div class="container mx-auto my-4 px-4">
         <div v-if="messages.length">
-            <h3>Message</h3>
+            <h3>Messages</h3>
             <div v-for="(message, id) in messages" :key="id">
                 {{ message.id }} - {{ message.message }} - {{ message.time }}
             </div>
         </div>
         <div class="my-3">
             <div>
-                <input class="rounded-2xl" type="text" placeholder="Message">
+                <input v-model="message" class="rounded-2xl" type="text" placeholder="Message">
             </div>
             <div>
-                <button href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl my-3">
+                <button @click="send" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl my-3">
                     Send
                 </button>
             </div>
